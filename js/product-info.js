25fab30a088bfa3e8d27b59b7d9c2da130824a55
Florencia <florenciagonzalez25@gmail.com>
2021-09-09 22:49:02 -0300

    Final changes entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
 // ENTREGA 3
+// Para que aparezcan las imagenes en forma de galería.
 var productos = {};
 
 function showImagesGallery(array) {
@@ -23,6 +24,7 @@ function showImagesGallery(array) {
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+// Para mostrar la información del producto.
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
@@ -45,57 +47,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
     });
 
-    document.getElementById("enviarComentario").addEventListener("click", function(){
-        let newCommentDescription = document.getElementById("addNewComment").value;
-        let newCommentScore = document.getElementById("selectStars").value;
-
-        let error = "";
-
-        if (newCommentDescription.length == 0 || newCommentScore == 0) {
-            error += `<FONT FACE="arial" SIZE=2 COLOR="red"> El comentario debe tener un mensaje y una puntuación.</FONT>`
-        } else {    
-        error += `Comentario enviado con éxito.`
-        document.getElementById("addNewComment").value = "";
-        document.getElementById("selectStars").value = 0;
-    }
-
-        document.getElementById("mensajeEnviado").innerHTML = error;
-       
-       /* 
-        let newCommentDescription = document.getElementById("addNewComment").value;
-        let scoreSelect = document.getElementById("Puntuacionstars").value;
-        let commentUser = localStorage.getItem("userloggedin");
-
-
-       // let DateTime = new Date().toLocaleString(); // TO CHECK
-
-
-
-/*        var stars = '';
-
-        for(var index = 0; index < score; index++)
-            stars += '<span class="fa fa-star" style="color:orange"/>';
-        
-        document.getElementById("comments-container").innerHTML += `
-    <a class="list-group-item list-group-item-action">
-        <div class="row">
-            <div class="col-3">
-            `+ stars + `
-            </div>
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <h6 class="mb-1">`+ commentUser + `<small class="text-muted">` + " - " + DateTime + `</small></h6>
-                </div>
-                <small class="text-muted">` + commentDescription + `</small>
-            </div>
-        </div>
-    </a>`
-
-*/  
-
-})
-
-
+    // Para mostrar los comentarios ya existentes.
     fetch(PRODUCT_INFO_COMMENTS_URL)
         .then(infocomments => infocomments.json())
         .then(data => {
@@ -109,10 +61,10 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
                 var stars = '';
 
-                for(var index = 0; index < score; index++) {
+                for (var index = 0; index < score; index++) {
                     stars += '<span class="fa fa-star" style="color:orange"/>';
                 }
-                
+
                 document.getElementById("comments-container").innerHTML += `
             <a class="list-group-item list-group-item-action">
                 <div class="row">
@@ -129,4 +81,53 @@ document.addEventListener("DOMContentLoaded", function (e) {
             </a>`
             }
         });
+
+        // Función para que no me permita continuar en caso de no completar ambos (comentario + puntuación)
+        document.getElementById("enviarComentario").addEventListener("click", function () {
+            let newCommentDescription = document.getElementById("addNewComment").value;
+            let newCommentScore = document.getElementById("selectStars").value;
+    
+            let error = "";
+    
+            if (newCommentDescription.length == 0 || newCommentScore == 0) {
+                error += `<FONT FACE="arial" SIZE=2 COLOR="red"> El comentario debe tener un mensaje y una puntuación.</FONT>`
+            } else {
+                error += `Comentario enviado con éxito.`
+    
+                //DESAFIATE ENTREGA 3 - Para que el comentario que enviamos se agregue a la lista de comentarios.
+                let newCommentDescription = document.getElementById("addNewComment").value;
+                let scoreSelect = document.getElementById("selectStars").value;
+                let commentUser = localStorage.getItem("userloggedin");
+                let DateTime = new Date().toLocaleString(); // TO CHECK
+    
+                let stars = '';
+    
+                for (var index = 0; index < scoreSelect; index++) {
+                    stars += '<span class="fa fa-star" style="color:orange"/>';
+                }
+    
+                let showNewComment = "";
+                showNewComment += `
+        <a class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                `+ stars + `
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h6 class="mb-1">`+ commentUser + `<small class="text-muted">` + " - " + DateTime + `</small></h6>
+                    </div>
+                    <small class="text-muted">` + newCommentDescription + `</small>
+                </div>
+            </div>
+        </a>`
+                document.getElementById("comments-container").innerHTML += showNewComment;
+    
+            // Entrega 3 - Para que al enviar el comentario se vacíen los campos.
+                document.getElementById("addNewComment").value = "";
+                document.getElementById("selectStars").value = 0;
+            }
+    
+            document.getElementById("mensajeEnviado").innerHTML = error;
+        })
 });
