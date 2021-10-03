cce7d426a92358e39cf4cbbebd78581cec8c8f0a
Florencia <florenciagonzalez25@gmail.com>
2021-10-03 20:29:47 -0300

    Entrega 4

--- js/product-info.js
+++ js/product-info.js
@@ -82,33 +82,33 @@ document.addEventListener("DOMContentLoaded", function (e) {
             }
         });
 
-        // Función para que no me permita continuar en caso de no completar ambos (comentario + puntuación)
-        //DESAFIATE ENTREGA 3 - Para que el comentario que enviamos se agregue a la lista de comentarios.
-        document.getElementById("enviarComentario").addEventListener("click", function () {
-            let newCommentDescription = document.getElementById("addNewComment").value;
-            let newCommentScore = document.getElementById("selectStars").value;
-    
-            let error = "";
-    
-            if (newCommentDescription.length == 0 || newCommentScore == 0) {
-                error += `<FONT FACE="arial" SIZE=2 COLOR="red"> El comentario debe tener un mensaje y una puntuación.</FONT>`
-            } else {
-                error += `Comentario enviado con éxito.`
-    
+    // Función para que no me permita continuar en caso de no completar ambos (comentario + puntuación)
+    //DESAFIATE ENTREGA 3 - Para que el comentario que enviamos se agregue a la lista de comentarios.
+    document.getElementById("enviarComentario").addEventListener("click", function () {
+        let newCommentDescription = document.getElementById("addNewComment").value;
+        let newCommentScore = document.getElementById("selectStars").value;
 
-                let newCommentDescription = document.getElementById("addNewComment").value;
-                let scoreSelect = document.getElementById("selectStars").value;
-                let commentUser = localStorage.getItem("userloggedin");
-                let DateTime = new Date().toLocaleString();
-    
-                let stars = '';
-    
-                for (var index = 0; index < scoreSelect; index++) {
-                    stars += '<span class="fa fa-star" style="color:orange"/>';
-                }
-    
-                let showNewComment = "";
-                showNewComment += `
+        let error = "";
+
+        if (newCommentDescription.length == 0 || newCommentScore == 0) {
+            error += `<FONT FACE="arial" SIZE=2 COLOR="red"> El comentario debe tener un mensaje y una puntuación.</FONT>`
+        } else {
+            error += `Comentario enviado con éxito.`
+
+
+            let newCommentDescription = document.getElementById("addNewComment").value;
+            let scoreSelect = document.getElementById("selectStars").value;
+            let commentUser = localStorage.getItem("userloggedin");
+            let DateTime = new Date().toLocaleString();
+
+            let stars = '';
+
+            for (var index = 0; index < scoreSelect; index++) {
+                stars += '<span class="fa fa-star" style="color:orange"/>';
+            }
+
+            let showNewComment = "";
+            showNewComment += `
         <a class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
@@ -122,13 +122,54 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 </div>
             </div>
         </a>`
-                document.getElementById("comments-container").innerHTML += showNewComment;
-    
+            document.getElementById("comments-container").innerHTML += showNewComment;
+
             // Entrega 3 - Para que al enviar el comentario se vacíen los campos.
-                document.getElementById("addNewComment").value = "";
-                document.getElementById("selectStars").value = 0;
+            document.getElementById("addNewComment").value = "";
+            document.getElementById("selectStars").value = 0;
+        }
+
+        document.getElementById("mensajeEnviado").innerHTML = error;
+    })
+
+
+
+    // Entrega 4 - Productos relacionados
+    fetch(PRODUCT_INFO_URL)
+        .then(info => info.json())
+        .then(data => {
+            for (let i = 0; i < data.relatedProducts.length; i++) {
+                let relatedProducts = data.relatedProducts[i];
+
+                fetch(PRODUCTS_URL)
+                    .then(info => info.json())
+                    .then(data => {
+
+                        let namerelatedProducts = data[relatedProducts].name
+                        let descriprelatedProducts = data[relatedProducts].description
+                        let imgrelatedProducts = data[relatedProducts].imgSrc
+                        let pricerelatedProducts = data[relatedProducts].currency + " " + data[relatedProducts].cost;
+
+
+                        let relatedProd = "";
+                        relatedProd += `
+            <div class="col-lg-3 col-md-4 col-6">
+                <div>
+                    <img class="img-fluid img-thumbnail" src="` + imgrelatedProducts + `"> 
+                    <h7 class="mb-1">`+ namerelatedProducts + `</h7>
+                    <div>
+                    <small class="text-muted">` + descriprelatedProducts + `</small>
+                </div>
+                <div>
+                    <small class="text-muted">` + pricerelatedProducts + `</small>
+                </div>
+                </div>
+            </div>
+            `
+                        document.getElementById("relatedProducts").innerHTML += relatedProd;
+                    })
             }
-    
-            document.getElementById("mensajeEnviado").innerHTML = error;
-        })
+
+        });
 });
+
